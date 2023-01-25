a747cbf1e1e55620d7207deb90ad45a00962ee4a
Karos <mail@wzl1.top>
2023-01-25 21:18:50 +0800

    1-25 update mannger.sql

--- back-yand/sql/mannger.sql
+++ back-yand/sql/mannger.sql
@@ -1,34 +1,37 @@
-create table mannger.article
+create table article
 (
-    id            varchar(58)                       not null comment 'id'
+    id                  varchar(256)                       not null comment 'id'
         primary key,
-    userId        bigint                             not null comment '所属用户ID',
-    userArticleid bigint   default 0                 not null comment '用户文章ID',
-    articleTitle  varchar(256)                       null,
-    articleUrl    varchar(256)                       not null comment '文章地址',
-    type          tinyint  default 0                 null comment '文章分类',
-    isPublic      tinyint  default 0                 not null comment '是否公开（0为否，1为公开）',
-    viewNum       bigint   default 0                 not null comment '浏览量',
-    IP            varchar(256)                       not null comment 'IP地址',
-    thumbNum      bigint   default 0                 not null comment '点赞量',
-    createTime    datetime default CURRENT_TIMESTAMP not null comment '注册时间',
-    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete      tinyint  default 0                 not null comment '是否删除（0否，1是）'
+    userId              bigint                             not null comment '所属用户ID',
+    userArticleid       bigint   default 0                 not null comment '用户文章ID',
+    articleTitle        varchar(256)                       null,
+    articleIntroduction int                                null,
+    articleUrl          varchar(256)                       not null comment '文章地址',
+    type                tinyint  default 0                 null comment '文章分类',
+    labelList           varchar(1024)                      null,
+    isPublic            tinyint  default 0                 not null comment '是否公开（0为否，1为公开）',
+    viewNum             bigint   default 0                 not null comment '浏览量',
+    IP                  varchar(256)                       not null comment 'IP地址',
+    thumbNum            bigint   default 0                 not null comment '点赞量',
+    createTime          datetime default CURRENT_TIMESTAMP not null comment '注册时间',
+    updateTime          datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete            tinyint  default 0                 not null comment '是否删除（0否，1是）'
 )
     comment '文章表';
 
-create table mannger.articlehistory
+create table articlehistory
 (
     id         varchar(256)                       not null comment '文章id',
     articleUrl varchar(256)                       not null comment '文章地址',
     ip         varchar(256)                       not null comment 'ip地址',
     version    bigint                             not null comment '文章',
     updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除（0否，1是）'
+    isDelete   tinyint  default 0                 not null comment '是否删除（0否，1是）',
+    label      varchar(256)                       null
 )
     comment '文章历史';
 
-create table mannger.articlethumbrecords
+create table articlethumbrecords
 (
     id        int auto_increment
         primary key,
@@ -40,11 +43,11 @@ create table mannger.articlethumbrecords
 )
     comment '文章点赞记录表';
 
-create table mannger.common
+create table common
 (
     attribute   varchar(256)                         not null comment '属性'
         primary key,
-    value       varchar(256)                         null comment '值',
+    value       varchar(512)                         null comment '值',
     comment     varchar(256)                         null,
     type        tinyint(1) default 0                 not null comment '''0 文本型 1 文件上传型',
     createdTime datetime   default CURRENT_TIMESTAMP null comment '创建或更新事件',
@@ -53,7 +56,7 @@ create table mannger.common
 )
     comment '普通配置表';
 
-create table mannger.type
+create table type
 (
     id       tinyint auto_increment comment '''ID'''
         primary key,
@@ -65,17 +68,17 @@ create table mannger.type
         unique (id)
 );
 
-create table mannger.user
+create table user
 (
     id           bigint auto_increment comment 'id'
         primary key,
     userName     varchar(256)                           null comment '用户昵称',
     userAccount  varchar(256)                           not null comment '账号',
-    userAvatar   varchar(256)                          null comment '用户头像',
+    userAvatar   varchar(1024)                          null comment '用户头像',
     userMail     varchar(256)                           null comment '用户邮箱',
     gender       tinyint                                null comment '性别',
     userRole     varchar(256) default 'user'            not null comment '用户角色：user / admin',
-    userPassword varchar(256)                           not null comment '密码',
+    userPassword varchar(512)                           not null comment '密码',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     isDelete     tinyint      default 0                 not null comment '是否删除',
