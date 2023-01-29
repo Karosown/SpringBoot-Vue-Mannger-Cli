37f4158f4fddc52f09d658cbd02128e232736c70
Karos <mail@wzl1.top>
2023-01-29 14:12:36 +0800

    2023-1-28 修复已知BUG

---
+++ back-yand/src/main/java/com/karos/project/service/CommonService.java
@@ -0,0 +1,13 @@
+package com.karos.project.service;
+
+import com.karos.project.model.entity.Common;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 30398
+* @description 针对表【common(普通配置表)】的数据库操作Service
+* @createDate 2023-01-17 07:53:55
+*/
+public interface CommonService extends IService<Common> {
+
+}
